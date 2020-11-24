474a3cfec2e0b5b107c5db91dbb77f2c8633a894
Your Name <you@example.com>
2020-11-24 11:46:36 +0530

    commit1

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,36 @@ import '../styles/App.css';
 
 const App = () => {
   // write your code here 
+  const [countdown,setCountDown] = useState(0);
+  const [timer,setTimer] = useState(false);
+  const handleKeyDown =(event) =>{
+    if(event.keyCode === 13){
+      setTimer(true);
+    event.target.value=null;
+    }
+    
+  }
 
+  const handleChange = (event) =>{
+    setCountDown(Math.trunc(event.target.value));
+    setTimer(false);
+  }
+  useEffect(()=>{
+    if(timer && countdown>0){
+      setTimeout(()=>{
+        setCountDown(countdown-1);
+      },1000)
+    } 
+  })
   return (
     <div className="wrapper">
       <div id="whole-center">
         <h1>
-          Reverse countdown for<input id="timeCount" onKeyDown={/* callback function */} /> sec.
+          Reverse countdown for
+          <input id="timeCount" onKeyDown={handleKeyDown} onChange={handleChange} /> sec.
         </h1>
       </div>
-      <div id="current-time">{/* remaining time */}</div>
+      <div id="current-time">{countdown}</div>
     </div>
   )
 }
